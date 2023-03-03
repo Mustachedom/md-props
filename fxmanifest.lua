88179b777ba7285b053f1900d4a5ddd7db7eaac8
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2023-03-02 16:45:27 -0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'md-props'
+description 'prop spawn by vector'
+version '1.0.2'
+
+client_script {
+    'client.lua',
+}
+
+
+shared_script {
+    'config.lua',
+    -- '@es_extended/imports.lua',    --- Uncomment if using ESX
+}
+
+dependencies {
+}
